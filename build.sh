c802339662e0037876fbe364536f806f7bd8b265
Spencer E <spencer.g.evans@gmail.com>
2024-12-16 13:41:19 -0500

    changed to cmake build instead of using xcode

--- build.sh
+++ build.sh
@@ -1,10 +1,5 @@
 #!/bin/zsh
 
-# Run xcodebuild
-xcodebuild clean build -project "Gyokuro.xcodeproj" -scheme "Gyokuro"
+cmake -S . -B build
 
-# Check if the build succeeded
-if [[ $? -ne 0 ]]; then
-  echo "❌ Build failed. Please check the error messages above." >&2
-  exit 1
-fi
+cmake --build build
--- run.sh
+++ run.sh
@@ -1,12 +1,3 @@
 #!/bin/zsh
 
-# Run xcodebuild
-xcodebuild build -project "Gyokuro.xcodeproj" -scheme "Gyokuro"
-
-# Check if the build succeeded
-if [[ $? -ne 0 ]]; then
-  echo "❌ Build failed. Please check the error messages above." >&2
-  exit 1
-fi
-
-open "./DerivedData/Gyokuro/Build/Products/Debug/Gyokuro"
+open build/Gyokuro
